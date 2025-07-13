53ed2e6ad3aa3a41544e443a14b94ea91b388c09
Andersonteixeira-dev <anderson_at@hotmail.com>
2025-07-13 00:07:39 -0300

    reforma

--- src/components/RotaProtegida.jsx
+++ src/components/RotaProtegida.jsx
@@ -15,20 +15,4 @@ function RotaProtegida({ children }) {
 }
 
 
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-function RotaProtegida({ children }) {
-    const token = localStorage.getItem('authToken');
-
-    if (!token) {
-        
-        return <Navigate to="/login" replace />;
-    }
-
-    
-    return children;
-}
-
-
 export default RotaProtegida;
